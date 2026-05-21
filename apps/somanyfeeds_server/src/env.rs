b388ea160e857d8db784682b2c545e23f27792e4
Damien Le Berrigaud <git@itsdamo.com>
2026-05-21 14:46:48 -0600

    Move env tests into `env_tests.rs`.

--- apps/somanyfeeds_server/src/env.rs
+++ apps/somanyfeeds_server/src/env.rs
@@ -13,32 +13,3 @@ where
         Err(_) => default,
     }
 }
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use std::env;
-
-    #[test]
-    fn test_load_env_num_default() {
-        let key = "TEST_ENV_VAR_DEFAULT";
-        unsafe { env::remove_var(key); }
-        assert_eq!(load_env_num(key, 42), 42);
-    }
-
-    #[test]
-    fn test_load_env_num_present() {
-        let key = "TEST_ENV_VAR_PRESENT";
-        unsafe { env::set_var(key, "123"); }
-        assert_eq!(load_env_num(key, 42), 123);
-        unsafe { env::remove_var(key); }
-    }
-
-    #[test]
-    #[should_panic(expected = "TEST_ENV_VAR_INVALID must be a valid number")]
-    fn test_load_env_num_invalid() {
-        let key = "TEST_ENV_VAR_INVALID";
-        unsafe { env::set_var(key, "abc"); }
-        load_env_num::<i32>(key, 42);
-    }
-}
